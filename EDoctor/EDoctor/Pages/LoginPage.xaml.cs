1e7eeaecd8c72b308e48727bb0ff16d4d28a1115
TeziTaya <tejbir1997.singh@gmail.com>
2021-05-13 11:43:23 +0530

    Initial app commit

---
+++ EDoctor/EDoctor/Pages/LoginPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using EDoctor.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EDoctor.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            var vm = new LoginViewModel();
+            this.BindingContext = vm;
+            vm.DisplayInvalidLoginPrompt += () => DisplayAlert("Error", "Invalid Login, try again", "OK");
+            InitializeComponent();
+
+            Email.Completed += (object sender, EventArgs e) =>
+            {
+                Password.Focus();
+            };
+
+            Password.Completed += (object sender, EventArgs e) =>
+            {
+                vm.SubmitCommand.Execute(null);
+            };
+            var registerNav = new TapGestureRecognizer();
+            registerNav.Tapped += registernav_tap_Tapped;
+            signUPLabel.GestureRecognizers.Add(registerNav);
+        }
+        private async void registernav_tap_Tapped(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new RegisterPage());
+        }
+    }
+}
